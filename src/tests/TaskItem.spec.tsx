b088fb2bd6387c5e514dd389ba5331ee1f7cee54
unknown <leonideko1@gmail.com>
2024-04-19 20:17:47 +0300

    fix test

---
+++ src/tests/TaskItem.spec.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import TaskItem from "../components/TaskItem";
+
+describe("TaskItem", () => {
+  test("renders TaskItem component", () => {
+    render(
+      <TaskItem task={{ id: "1", title: "Test Task", completed: false, description: "" }} deleteTask={function (id: string): void {
+        throw new Error("Function not implemented.");
+      } } toggleTaskCompletion={function (id: string): void {
+        throw new Error("Function not implemented.");
+      } }  />
+    );
+    expect(screen.getByText("Test Task")).toBeInTheDocument();
+  });
+});
