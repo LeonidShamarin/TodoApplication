8004ad669e6bb1182eeabccaa81c205466356b3e
unknown <leonideko1@gmail.com>
2024-04-19 20:34:06 +0300

    delete import

--- src/tests/TaskItem.spec.tsx
+++ src/tests/TaskItem.spec.tsx
@@ -5,11 +5,20 @@ import TaskItem from "../components/TaskItem";
 describe("TaskItem", () => {
   test("renders TaskItem component", () => {
     render(
-      <TaskItem task={{ id: "1", title: "Test Task", completed: false, description: "" }} deleteTask={function (id: string): void {
-        throw new Error("Function not implemented.");
-      } } toggleTaskCompletion={function (id: string): void {
-        throw new Error("Function not implemented.");
-      } }  />
+      <TaskItem
+        task={{
+          id: "1",
+          title: "Test Task",
+          completed: false,
+          description: "",
+        }}
+        deleteTask={function (id: string): void {
+          throw new Error("Function not implemented.");
+        }}
+        toggleTaskCompletion={function (id: string): void {
+          throw new Error("Function not implemented.");
+        }}
+      />
     );
     expect(screen.getByText("Test Task")).toBeInTheDocument();
   });
